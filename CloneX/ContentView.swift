78a8175dbf54bb946a0e9885df4077c1ed7e63a8
tttuuu1234 <yidongyi8@gmail.com>
2024-10-20 21:55:22 +0900

    Top画面の作成

--- CloneX/ContentView.swift
+++ CloneX/ContentView.swift
@@ -11,18 +11,103 @@ import SwiftData
 struct ContentView: View {
     
     var body: some View {
-        VStack(content: {
-            Button(action: {}, label: {
-                HStack(content: {
-                    Image(systemName: "star")
-                    Text("Googleのアカウントで続ける")
-                        .font(.system(size: 16))
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(content: {
+                Text("X")
+                Spacer()
+                
+                Text("「いま」起きていること\nをみつけよう")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                Spacer()
+                
+                Group(content: {
+                    Button(action: {}, label: {
+                        HStack(content: {
+                            Image(systemName: "star")
+                            Text("Googleのアカウントで続ける")
+                                .font(.system(size: 16))
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            
+                        })
+                    })
+                    .buttonStyle(RoundedButtonStyle())
+                    .padding(.bottom, 8)
                     
+                    Button(action: {}, label: {
+                        HStack(content: {
+                            Image(systemName: "star")
+                            Text("Appleのアカウントで続ける")
+                                .font(.system(size: 16))
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            
+                        })
+                    })
+                    .buttonStyle(RoundedButtonStyle())
+                    .padding(.bottom, 16)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(.gray)
+                            .frame(height: 0.8)
+                        Text("または")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        Capsule()
+                            .fill(.gray)
+                            .frame(height: 0.8)
+                    }
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .padding(.horizontal, 16)
+                    
+                    NavigationLink {
+                        
+                    } label: {
+                        Button(action: {}, label: {
+                            HStack(content: {
+                                Text("アカウントを作成")
+                                    .font(.system(size: 16))
+                                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                
+                            })
+                        })
+                        .buttonStyle(RoundedButtonStyle())
+                    }
+                    .padding(.vertical, 16)
+                    
+                    HStack {
+                        Text("登録すると") +
+                        Text("利用規約").foregroundColor(Color(.systemBlue))  +
+                        Text("、")  +
+                        Text("プライバシポリシー").foregroundColor(.blue)  +
+                        Text("、")  +
+                        Text("Cookieの使用").foregroundColor(.blue)  +
+                        Text("に同意したものとみなされます。")
+                    }
+                    .font(.caption)
+                    .fontWeight(.regular)
+                    .lineSpacing(6)
+                    .padding(.top, 8)
+                    
+                    
+                    HStack {
+                        Text("アカウントをお持ちの方は")
+                            .foregroundColor(.black.opacity(0.8))
+                            .font(.footnote)
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("ログイン")
+                                .font(.footnote)
+                        }
+                    }
+                    .padding(.vertical, 40)
                 })
+                .padding(.horizontal, 30)
             })
-            .buttonStyle(RoundedButtonStyle())
-        })
+            .padding(.bottom, 10)
+        }
     }
 }
 
